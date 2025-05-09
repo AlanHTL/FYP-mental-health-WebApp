d116e90f5d3bf2df607b39a3711cf73dfd06b04c
AlanHTL <165019180+AlanHTL@users.noreply.github.com>
2025-05-09 23:30:55 +0800

    final

--- backend/.venv/Scripts/activate.bat
+++ backend/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\lun51\Desktop\FYP 7-5-2025 full stack\FYP-share\backend\.venv"
+set "VIRTUAL_ENV=C:\Users\lun51\Desktop\fyp-08052025 version\FYP-share\backend\.venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
