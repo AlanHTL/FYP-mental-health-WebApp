770e27356039df3f938f3798f83d29c90d132c5d
AlanHTL <165019180+AlanHTL@users.noreply.github.com>
2025-05-11 23:24:22 +0800

    ok

--- backend/database.py
+++ backend/database.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
-MONGO_DB = os.getenv("MONGO_DB", "mental_health_db")
+MONGO_URI = os.getenv("MONGO_URI")
+MONGO_DB = os.getenv("MONGO_DB")
 
 client = AsyncIOMotorClient(MONGO_URI)
 db = client[MONGO_DB]
